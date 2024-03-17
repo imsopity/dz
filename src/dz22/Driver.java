43a527a7f088f62d195a8da583d27415a2f9be74
user <ggg@gmail.com>
2024-03-17 11:44:20 +0300

    сделал дз 2.2

--- src/dz22/Driver.java
+++ src/dz22/Driver.java
@@ -1,4 +1,28 @@
 package dz22;
 
 public class Driver {
+    public static void main (String[] args) {
+        driving_experience driver = new driving_experience();
+        driver.driexp = 2;
+        driver.category = "B";
+        System.out.println("Стаж вождения: " +driver.driexp);
+        System.out.println("Категория вождения: " +driver.category);
+
+        int keysswitch = 0;
+        // 0 - ключи от bmw e34
+        // 1 - ключи от ford sierra
+        // 2 - ключи от bmw e36
+        switch (keysswitch) {
+            case 0:
+                System.out.println("Выбраны ключи от bmw e34");
+                break;
+            case 1:
+                System.out.println("Выбраны ключи от ford sierra");
+                break;
+            case 2:
+                System.out.println("Выбраны ключи от bmw e34");
+            default:
+                System.out.println("Выбраны неверные ключи");
+    }
+    }
 }
---
+++ src/dz23/driver_can.java
@@ -0,0 +1,4 @@
+package dz23;
+
+public class driver_can {
+}
