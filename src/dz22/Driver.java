822e95541a54fa0632b3027dded9bb1c9bb6d55e
user <ggg@gmail.com>
2024-03-17 22:58:56 +0300

    сделал остальные задания

--- src/dz22/Driver.java
+++ src/dz22/Driver.java
@@ -1,5 +1,7 @@
 package dz22;
 
+import dz22.driving_experience;
+
 import java.util.Scanner;
 
 public class Driver {
@@ -7,35 +9,60 @@ public class Driver {
         driving_experience driver = new driving_experience();
         driver.driexp = 2;
         driver.category = "B";
+        driver.carplace = 5;
+        driver.tank = 25;
         System.out.println("Стаж вождения: " +driver.driexp);
         System.out.println("Категория вождения: " +driver.category);
 
-        int keysswitch = 0;
+        Scanner bscan = new Scanner(System.in);
+        System.out.print("Выберите ключи: ");
+        int keyss = bscan.nextInt();
         // 0 - ключи от bmw e34
         // 1 - ключи от ford sierra
         // 2 - ключи от bmw e36
-        switch (keysswitch) {
-            case 0:
-                System.out.println("Выбраны ключи от bmw e34");
-                break;
-            case 1:
-                System.out.println("Выбраны ключи от ford sierra");
-                break;
-            case 2:
-                System.out.println("Выбраны ключи от bmw e34");
-            default:
-                System.out.println("Выбраны неверные ключи");
-    }
-//        Scanner scan = new Scanner(System.in);
-//        System.out.print("Введите категорию прав: ");
-//        String licence = scan.nextLine();
-//        String licence;
-//        int licence = driver.driexp;
+        if (keyss == 0) {
+            System.out.println("Выбраны ключи от bmw e34");
+        } else if (keyss == 1) {
+            System.out.println("Выбраны ключи от ford sierra");
+        } else if (keyss == 2) {
+            System.out.println("Выбраны ключи от bmw e36");
+        } else if (keyss <= 3) {
+            System.out.println("Выбраны неверные ключи");
+            return;
+        }
+
         if(driver.category == "B") {
             System.out.println("Категория прав подходит");
         }
         else if (driver.category != "B") {
             System.out.println("Категория прав не подходит");
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество пассажиров: ");
+        int seats = scanner.nextInt();
+        if(seats >= driver.carplace) {
+            System.out.println("Количество пассажиров не подходит");
+            return;
+        }
+        else if (seats <= driver.carplace ) {
+        }
+        System.out.println("Кол-во топлива: " +driver.tank +" литров");
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Хотите заправить авто?: ");
+//        1 - да
+//        2 - нет
+        int fuel = scanner.nextInt();
+        if(fuel == 1) {
+            System.out.println("Введите количество топлива которое хотите залить: ");
+        }
+        else if (fuel == 2 ) {
+            return;
+        }
+        Scanner d = new Scanner(System.in);
+        int howfuel = scanner.nextInt();
+        int how = Integer.parseInt(String.valueOf(driver.tank));
+        int result = howfuel + how;
+            System.out.println("Теперь количество топлива равно: " +result);
+        }
     }
-}
--- src/dz22/driving_experience.java
+++ src/dz22/driving_experience.java
@@ -3,5 +3,7 @@ package dz22;
 public class driving_experience {
     public int driexp;
     public String category;
+    public int carplace;
+    public int tank;
 
 }
